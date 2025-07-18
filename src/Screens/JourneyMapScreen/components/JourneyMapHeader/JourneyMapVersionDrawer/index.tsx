5f5f91d5450e99f623cc1bfd79de0714480d55e4
Narek97 <narek.babayan.97.27@gmail.com>
2025-07-18 12:28:52 +0400

    disable not allowed versions

--- src/Screens/JourneyMapScreen/components/JourneyMapHeader/JourneyMapVersionDrawer/index.tsx
+++ src/Screens/JourneyMapScreen/components/JourneyMapHeader/JourneyMapVersionDrawer/index.tsx
@@ -27,6 +27,23 @@ interface IVersionDrawer {
 }
 dayjs.extend(fromNow);
 
+const DISABLED_VERSIONS = [
+  1223, 1218, 1214, 1207, 1203, 1187, 1186, 1174, 1119, 1114, 1110, 1108, 1100, 1102, 1089, 1077,
+  1046, 1045, 1044, 1039, 1037, 1036, 1033, 1011, 1009, 1008, 1007, 1004, 1003, 998, 996, 995, 993,
+  994, 992, 990, 986, 985, 984, 983, 982, 896, 891, 886, 876, 875, 866, 865, 863, 860, 858, 857,
+  854, 850, 838, 839, 831, 819, 821, 809, 808, 806, 803, 800, 801, 796, 797, 795, 788, 789, 787,
+  786, 776, 775, 774, 771, 773, 767, 748, 747, 703, 692, 682, 679, 678, 676, 675, 674, 673, 672,
+  667, 635, 631, 628, 629, 630, 623, 621, 615, 612, 613, 611, 608, 609, 606, 607, 605, 599, 598,
+  596, 597, 595, 594, 592, 591, 588, 587, 584, 580, 581, 578, 579, 577, 576, 575, 574, 571, 570,
+  567, 560, 559, 557, 554, 555, 545, 546, 547, 543, 540, 534, 533, 529, 528, 526, 527, 521, 519,
+  520, 515, 513, 511, 512, 499, 492, 488, 489, 487, 479, 477, 476, 474, 470, 465, 466, 464, 463,
+  455, 456, 447, 445, 441, 439, 433, 434, 429, 421, 418, 414, 417, 412, 411, 408, 404, 387, 388,
+  384, 376, 374, 375, 368, 366, 365, 363, 361, 356, 355, 343, 341, 304, 296, 285, 282, 248, 249,
+  239, 224, 215, 217, 207, 206, 205, 192, 186, 181, 180, 178, 179, 166, 167, 161, 160, 152, 145,
+  143, 139, 138, 137, 129, 130, 127, 125, 124, 123, 122, 120, 118, 109, 108, 107, 105, 106, 102,
+  103, 99, 88, 86, 87, 84, 81, 79, 78, 74, 70, 64, 34, 32, 31, 21, 19, 17, 9, 2, 3,
+];
+
 const VersionDrawer: FC<IVersionDrawer> = ({ mapID, onHandleClose }) => {
   const { updateJourneyMapVersion, updateJourneyMap } = useJourneyMapStore();
 
@@ -180,9 +197,10 @@ const VersionDrawer: FC<IVersionDrawer> = ({ mapID, onHandleClose }) => {
         }}>
         {renderedVersionsData.length ? (
           <ul data-testid="versions-list-ul" ref={childRef}>
-            {renderedVersionsData.map(version => (
+            {renderedVersionsData.map((version, index) => (
               <li key={version.id}>
                 <VersionCard
+                  isDisabled={DISABLED_VERSIONS.includes(version.id) || index === 0}
                   version={version}
                   onHandleRestoreVersion={onHandleRestoreVersion}
                   onHandleDeleteVersion={onHandleToggleDeleteVersion}
