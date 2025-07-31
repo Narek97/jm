184eed6bbbaa72ba9513429ae1e36cba17dc67a4
melineabrahamyan <abrahamyan.meline@gmail.com>
2025-07-31 17:48:40 +0400

    changed to tailwind

--- src/Screens/PersonaGroupScreen/components/PersonaAiModal/InfoIcon/index.tsx
+++ src/Screens/PersonaGroupScreen/components/PersonaAiModal/InfoIcon/index.tsx
@@ -1,13 +1,9 @@
-import './style.scss';
 import { WuTooltip } from '@npm-questionpro/wick-ui-lib';
 
 const InfoIcon = () => {
   return (
-    <WuTooltip
-      className="wu-tooltip-content"
-      content="Add behavioural and usage context"
-      position="right">
-      <span className="wm-info"></span>
+    <WuTooltip content="Add behavioural and usage context" position="right">
+      <span className="wm-info text-[1.2rem] rounded-full cursor-pointer"></span>
     </WuTooltip>
   );
 };
